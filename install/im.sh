160d3b8a4298ee9bad9a534425d5a53e15bec92a
小樱 <1265578519@qq.com>
2021-05-03 04:56:54 +0800

    关闭遇到404错误则退出安装
    关闭遇到404错误则退出安装

--- install/im.sh
+++ install/im.sh
@@ -11,12 +11,12 @@ if [ $# != 1 ] ;then
         echo "usage: download_master.sh version"
         exit 1
 fi
-wget -O cdnbest-master.tar.gz $DOWNLOAD_PREFIX/cdnbest-master-$1-$OS.tar.gz
-ret=$?
-if [ $ret != 0 ]; then
-        exit $ret
-fi
-tar xzf cdnbest-master.tar.gz
+#wget -O cdnbest-master.tar.gz $DOWNLOAD_PREFIX/cdnbest-master-$1-$OS.tar.gz
+#ret=$?
+#if [ $ret != 0 ]; then
+#        exit $ret
+#fi
+#tar xzf cdnbest-master.tar.gz
 echo "ready cdnbest-master for you..."
 yum -y install glibc.i686 wget tar bzip2
 wget $DOWNLOAD_PREFIX/cdnbest-master-$1-$OS.tar.gz.7z.001 -O cdnbest-master.tar.gz.001
@@ -24,8 +24,9 @@ wget $DOWNLOAD_PREFIX/cdnbest-master-$1-$OS.tar.gz.7z.002 -O cdnbest-master.tar.
 wget $DOWNLOAD_PREFIX/fix/p7zip_16.02_x86_linux_bin.tar.bz2 -O p7zip_16.02_x86_linux_bin.tar.bz2
 tar -xjvf p7zip_16.02_x86_linux_bin.tar.bz2
 cd p7zip_16.02/bin
-./7za x /tmp/cdnbest-master.tar.gz.001
+./7za -y x /tmp/cdnbest-master.tar.gz.001
 \cp -f cdnbest-master-*.tar.gz /tmp/cdnbest-master.tar.gz
+rm -rf cdnbest-master-*.tar.gz
 cd /tmp
 tar xzf cdnbest-master.tar.gz
 wget $DOWNLOAD_PREFIX/fix/common.sh -O cdnbest-master/shell/common.sh
